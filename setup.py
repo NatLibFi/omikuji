9c3b55de597d3844fcfb423f5637ab42e0963044
Juho Inkinen <34240031+juhoinkinen@users.noreply.github.com>
2026-06-12 11:24:36 +0300

    Fix setup fn import and milksnake setting

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from milksnake import setup
+from setuptools import setup
 from milksnake_tasks import build_native
 
-setup(milksnake_tasks=[build_native])
+setup(milksnake_tasks=[build_native], milksnake_universal=False)
